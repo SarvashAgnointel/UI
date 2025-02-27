5f81c44490048b7b2c9df0c490dc04d5d1e7d61f
SarvashAgnointel <sarvash@agnointel.ai>
2025-02-27 12:09:54 +0530

    load a loginvideo path from s3

--- src/Pages/SignInPage.tsx
+++ src/Pages/SignInPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import loginVideo from "../Assets/loginVideo.mp4";
+import loginVideo from "https://mediafiles-travelad.s3.eu-central-1.amazonaws.com/loginVideo.mp4";
 import Logo from '../Assets/Logo.svg';
 import { Link, useNavigate } from "react-router-dom";
 import { Input } from '../Components/ui/input';
